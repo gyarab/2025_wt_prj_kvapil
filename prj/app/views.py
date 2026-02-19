1bb1d0b6c5ad0d856f21e5378e877e040c473afd
Jakub Kvapil <toustovac.cz@gmail.com>
2026-02-19 23:45:32 +0100

    initial commit

---
+++ prj/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def render_home(request):
+    return render(request, "home.html")
+
+def render_about(request):
+    return render(request, "about.html")
