237a53e52a3987a569006aee51281ef38a562de6
Jakub Kvapil <kvapiljakub5@gmail.com>
2026-03-13 12:21:01 +0100

    further changes

--- prj/app/admin.py
+++ prj/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Movie, Director, Actor
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Director)
+class DirectorAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
